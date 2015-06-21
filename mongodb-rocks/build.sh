fdf6a9df9c5b381e16b64c71686dbd3b9e50b880
Ben Cessa <ben@pixative.com>
2015-06-21 11:01:49 -0500

    MongoDB + RocksDB
    RocksDB is write-optimized storage engine developed by Facebook.
    For more information:
    http://blog.parse.com/announcements/mongodb-rocksdb-parse/

---
+++ mongodb-rocks/build.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+# Describes the procedure to compile the mongo binaries
+
+# Deps
+apt-get install \
+  build-essential \
+  git-core \
+  lintian \
+  libpcre3 \
+  libpcre3-dev \
+  libboost-dev \
+  libboost-date-time-dev \
+  libboost-filesystem-dev \
+  libboost-program-options-dev \
+  libboost-system-dev \
+  libboost-thread-dev \
+  libpcap-dev \
+  libreadline-dev \
+  libssl-dev \
+  libbz2-dev \
+  libsnappy-dev \
+  libzlcore-dev \
+  rng-tools \
+  scons \
+  zlib1g-dev
+
+# RocksDB
+git clone https://github.com/facebook/rocksdb.git
+cd rocksdb
+git checkout mongorocks
+make static_lib
+make install
+cd ..
+
+# Mongo
+mkdir /var/tmp/rocks-build
+git clone https://github.com/mongodb-partners/mongo.git
+cd mongo
+git checkout v3.0-mongorocks
+scons mongod mongo mongos --64 --ssl --release -j 4 --prefix=/var/tmp/rocks-build --rocksdb=1 install
+cd ..
+
+# Pack
+tar -cvzf ~/mongo-rocks.tgz /var/tmp/rocks-build/bin/*
